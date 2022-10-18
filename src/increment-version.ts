940d90108ebc672efe5073f44dfda83fc1019124
jaysonboyer <2801672+jaysonboyer@users.noreply.github.com>
2022-10-18 07:35:06 -0700

    test

--- src/increment-version.ts
+++ src/increment-version.ts
@@ -5,17 +5,19 @@ import { readFileSync } from "fs";
 export function incrementVersion() {
   try {
     const eventPayload = JSON.parse(
-      readFileSync(process.env.GITHUB_EVENT_PATH || '').toString()
+      readFileSync(process.env.GITHUB_EVENT_PATH || "").toString()
     );
     const latestVersion = core.getInput("latest-version");
     const defaultReleaseType = core.getInput("default-release-type");
     const releaseType =
       eventPayload.pull_request?.labels
-        ?.find((label: { name: string }) =>
-          ["do-not-release", "major", "minor", "patch"].includes(
-            label.name.toLowerCase()
-          )
-        )
+        ?.find((label: { name: string }) => {
+          const findLabel = label.name.toLowerCase();
+          console.log({findLabel})
+          return ["do-not-release", "major", "minor", "patch"].includes(
+            findLabel
+          );
+        })
         ?.name.toLowerCase() || defaultReleaseType;
     if (!releaseType) {
       throw new Error(
@@ -31,9 +33,8 @@ export function incrementVersion() {
       core.setOutput("new-version", newVersion);
       console.log({ cleanVersion, releaseType, newVersion });
     }
-  }
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  catch (e: any) {
-    core.setFailed(e?.message || 'unknown error');
+  } catch (e: any) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    core.setFailed(e?.message || "unknown error");
   }
 }
