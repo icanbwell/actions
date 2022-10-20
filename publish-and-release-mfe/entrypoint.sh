5a7283c78a3e1585da3aa2733bcd49af7b8f1269
Tommy Morgan <tommy@tommymorgan.com>
2022-10-20 12:03:32 -0500

    TECHX-573 testing

--- publish-and-release-mfe/entrypoint.sh
+++ publish-and-release-mfe/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# version=$PREFIX$(jq .version package.json | sed 's/"//g')
+version=$PREFIX$(jq .version package.json | sed 's/"//g')
 # echo "::set-output name=version::$version"
 
 # yarn install --frozen-lockfile && yarn publish
 
 echo "result=IN THE SHELL SCRIPT" >> $GITHUB_OUTPUT
+echo "version=$version" >> $GITHUB_OUTPUT
